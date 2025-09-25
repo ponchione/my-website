931fa22bebbee3c43f75737520b574c0e3f6480d
Mitchell Ponchione <mitchell.ponchione@protonmail.com>
2025-09-25 10:39:24 -0400

    Revamped the about me

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -1,22 +1,29 @@
+import {Separator} from "@/components/ui/separator";
 
 export function AboutPage() {
     return (
         <section className={"mt-8"}>
             <div className="space-y-4 text-muted-foreground">
                 <p>
-                    I'm a senior full-stack software engineer with over eight years of experience
-                    building and modernizing enterprise systems across the insurance, banking,
-                    and government sectors. My expertise covers a wide range of frontend,
-                    backend, and cloud technologies, with a proven track record of
-                    architecting data platforms and revitalizing legacy systems.
+                    I'm Mitchell, a senior full-stack engineer who loves the craft of building.
+                    For over eight years, I’ve applied that passion to modernizing critical systems in the insurance,
+                    banking, and government sectors, specializing in untangling legacy code and architecting new data
+                    platforms.
                 </p>
+                <Separator />
                 <p>
-                    What has always drawn me to this field is the fundamental human drive to
-                    create. For me, software development is the ultimate medium for building
-                    something new and useful from the ground up. While I find deep satisfaction
-                    in the craft of engineering itself, the ultimate reward is seeing the
-                    solutions I build make a tangible, positive impact for the people who
-                    use them.
+                    While I enjoy the technical challenge, the real reward is seeing the software I build make
+                    a tangible, positive impact for the people who rely on it every day.
+                </p>
+
+                <Separator />
+
+                <p>
+                    First and foremost, I'm a husband and father to a growing family of (soon to be) five.
+                    That role has taught me invaluable lessons in patience and long-term planning.
+                    But the engineer in me still needs an outlet, which I find in building experimental projects
+                    with Go, or playing grand strategy games with friends. And when I need a complete reset, I find
+                    it in the focus and simplicity of rucking outdoors.
                 </p>
             </div>
         </section>
